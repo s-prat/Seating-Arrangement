a8ff458cc949dc8ac4c2adc3e270897d1006338d
Pratibha <psharma@atlassian.com>
2022-04-29 19:01:09 +0100

    added empty availableseats() method in service file

--- src/main/java/com/cinema/seating/SeatAllocationService.java
+++ src/main/java/com/cinema/seating/SeatAllocationService.java
@@ -5,4 +5,8 @@ public class SeatAllocationService {
     public String allocateSeats(int inputSeat) {
         return "A1 A2 A3";
     }
+
+    public boolean availableSeats(int i) {
+        return true;
+    }
 }
