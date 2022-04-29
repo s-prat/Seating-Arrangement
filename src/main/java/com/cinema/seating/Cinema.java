a812309dbab5370267605e704acb9c001f8e6a6f
Pratibha <psharma@atlassian.com>
2022-04-30 00:47:45 +0100

    added attributes to Ticket and Cinema class

--- src/main/java/com/cinema/seating/Cinema.java
+++ src/main/java/com/cinema/seating/Cinema.java
@@ -1,4 +1,14 @@
 package com.cinema.seating;
 
 public class Cinema {
+    private int totalSeatingCapacity;
+    private int noOfRows;
+    private int noOfColumns;
+
+    public Cinema(int totalSeatingCapacity, int noOfRows, int noOfColumns) {
+        this.totalSeatingCapacity = totalSeatingCapacity;
+        this.noOfColumns = noOfColumns;
+        this.noOfRows = noOfRows;
+    }
+
 }
--- src/main/java/com/cinema/seating/Ticket.java
+++ src/main/java/com/cinema/seating/Ticket.java
@@ -1,4 +1,25 @@
 package com.cinema.seating;
 
+import java.util.HashMap;
+
 public class Ticket {
+    private Cinema cinemaDetails;
+    private int[][] seatingArray;
+    private int currentAvailableSeats;
+    private HashMap rowMap;
+
+    public Ticket(Cinema cinemaDetails) {
+        this.cinemaDetails = cinemaDetails;
+    }
+
+    private HashMap<Character, Integer> getMap() {
+        rowMap = new HashMap<Character, Integer>();
+        rowMap.put('A', 5);
+        rowMap.put('B', 5);
+        rowMap.put('C', 5);
+        return rowMap;
+    }
+
+
+
 }
